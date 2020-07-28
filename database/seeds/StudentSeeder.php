345d7064cd5381fbddbbd852d0100e1e5d8a95f1
Silas Junior <silas.pereirajunior04@gmail.com>
2020-07-28 12:37:10 -0300

    Students => migration, model, controller, route, form request and seed created

---
+++ database/seeds/StudentSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Student;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Student::create([
+            'name' => \Faker\Factory::create()->name,
+            'email' => \Faker\Factory::create()->unique()->email,
+            'birth_date' => \Faker\Factory::create()->date('d-m-Y')
+        ]);
+
+        Student::create([
+            'name' => \Faker\Factory::create()->name ,
+            'email' => \Faker\Factory::create()->unique()->email,
+            'birth_date' => \Faker\Factory::create()->date('d-m-Y')
+        ]);
+
+        Student::create([
+            'name' => \Faker\Factory::create()->name ,
+            'email' => \Faker\Factory::create()->unique()->email,
+            'birth_date' => \Faker\Factory::create()->date('d-m-Y')
+        ]);
+    }
+}
